bfc69a29dd58181dffe1c214c5428efbd0d892b8
rizwannitk <61521011+rizwannitk@users.noreply.github.com>
2020-12-29 23:16:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,25 @@ model = pickle.load(open('rf.pkl', 'rb'))
 def hello():
     return 'Hello World'
 
+@app.route('/webhook', methods=['POST'])
+@cross_origin()
+def webhook():
 
+    req = request.get_json(silent=True, force=True)
 
+    #print("Request:")
+    #print(json.dumps(req, indent=4))
+
+    res = processRequest(req)
+
+    res = json.dumps(res, indent=4)
+    #print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+def processRequest(req):
+    fulfillmentText= "The Iris type seems to be..  {} !"
 if __name__ == '__main__':
     app.run()
 #if __name__ == '__main__':
