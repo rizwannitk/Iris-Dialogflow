7f79b3bc4e23dac7d3a17c2a7cfb599cea047362
rizwannitk <61521011+rizwannitk@users.noreply.github.com>
2021-01-01 10:28:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -45,40 +45,27 @@ def processRequest(req):
 	 
     intent = result.get("intent").get('displayName')
     
-    if (intent=='irisdata'):
-	parameters = result.get("parameters")
-        Petal_length=parameters.get("number")
-        Petal_width = parameters.get("number1")
-        Sepal_length=parameters.get("number2")
-        Sepal_width=parameters.get("number3")
-        int_features = [Petal_length,Petal_width,Sepal_length,Sepal_width]
+    if (intent=='final'):
+	#parameters = result.get("parameters")
+        #Petal_length=parameters.get("number")
+        #Petal_width = parameters.get("number1")
+        #Sepal_length=parameters.get("number2")
+        #Sepal_width=parameters.get("number3")
+        #int_features = [Petal_length,Petal_width,Sepal_length,Sepal_width]
     
-        final_features = [np.array(int_features)]
-        prediction = model.predict(final_features)
+        #final_features = [np.array(int_features)]
+        #prediction = model.predict(final_features)
     
-        output = round(prediction[0], 2)
+        #output = round(prediction[0], 2)
     
     	
-        if(output==0):
-            flowr = 'Setosa'
-    
-        if(output==1):
-            flowr = 'Versicolour'
-        
-        if(output==2):
-            flowr = 'Virginica'
        
-        fulfillmentText= "The Iris type seems to be..  {} !".format(flowr)
+        fulfillmentText= "The Iris type seems to be.. "
         #log.write_log(sessionID, "Bot Says: "+fulfillmentText)
         return {
             "fulfillmentText": fulfillmentText
         }
-    else:
-        fulfillmentText= "The is working"
-        #log.write_log(sessionID, "Bot Says: "+fulfillmentText)
-        return {
-            "fulfillmentText": fulfillmentText
-        } 
+    
 	       
 if __name__ == '__main__':
     app.run()
