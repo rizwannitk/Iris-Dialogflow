02b8758b6da65a8478afe67d544d445e8fd27b9a
rizwannitk <61521011+rizwannitk@users.noreply.github.com>
2021-01-01 09:38:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -17,13 +17,13 @@ def webhook():
 
     req = request.get_json(silent=True, force=True)
 
-    #print("Request:")
-    #print(json.dumps(req, indent=4))
+    print("Request:")
+    print(json.dumps(req, indent=4))
 
     res = processRequest(req)
 
     res = json.dumps(res, indent=4)
-    #print(res)
+    print(res)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
