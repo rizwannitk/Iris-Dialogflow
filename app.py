d813797c068f7ad061dbe6552b2480002d8f3ed6
rizwannitk <61521011+rizwannitk@users.noreply.github.com>
2021-01-01 08:47:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -32,11 +32,15 @@ def processRequest(req):
 	
     result = req.get("queryResult")
 
+    log.write_log(sessionID, "Bot Says: hello ")
+
     intent = result.get("intent").get('displayName')
+	
+	log.write_log(sessionID, "Bot Says: "+intent)
     
     if (intent=="final"):
         fulfillmentText= "The is working"
-        #log.write_log(sessionID, "Bot Says: "+fulfillmentText)
+        log.write_log(sessionID, "Bot Says: "+fulfillmentText)
         return {
             "fulfillmentText": fulfillmentText
         }
